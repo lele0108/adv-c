110e718a976f6aa1495b6378a3ef0224f4efd3d1
Jimmy Liu <sicong.liu98@gmail.com>
2015-09-27 15:16:52 -0700

    done

--- Test/Test/main.cpp
+++ Test/Test/main.cpp
@@ -1,7 +1,8 @@
-// Demetris Leadership Center (DLC) product lookup program
-// This program allows the user to enter a product number
-// and then displays the title, description, and price of
-// that product.
+// Homework One
+// Jimmy (Sicong) Liu 20076260
+// CIS 22B
+// September 27th 2015
+
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -10,7 +11,8 @@ using namespace std;
 // Function prototypes
 string getProdNum();
 int binarySearch (const string [], int, string);
-void displayProd(const double [], int);
+void displayProd(const double [], int, const string[]);
+void writeToFile(const string[], int);
 
 
 int main()
@@ -18,6 +20,8 @@ int main()
     // Variable initializatoin
     string names[100];
     double gpa[100];
+    string notFound[100];
+    int notFoundCounter = 0;
     
     // Reading in text file to array names
     string name;
@@ -25,9 +29,14 @@ int main()
     ifstream myfile("names.txt", ifstream::in);
     int arrayCounter = 0;
     while(myfile >> value >> name) {
-        gpa[arrayCounter] = value;
-        names[arrayCounter] = name;
-        arrayCounter++;
+        if (arrayCounter < 100) {
+            gpa[arrayCounter] = value;
+            names[arrayCounter] = name;
+            arrayCounter++;
+        } else {
+            cout << "The file contains more than 100 lines!";
+            break;
+        }
     }
     
     /*for (int i = 0; i < arrayCounter; i++) {
@@ -48,15 +57,19 @@ int main()
         index = binarySearch(names, arrayCounter, prodNum);
         
         // Display the results of the search.
-        if (index == -1)
+        if (index == -1) {
             cout << "That student was not found.\n";
+            notFound[notFoundCounter] = prodNum;
+            notFoundCounter++;
+        }
         else
-            displayProd(gpa, index);
+            displayProd(gpa, index, names);
         
         // Does the user want to do this again?
         cout << "Would you like to look up another student? (y/n) ";
         cin >> again;
     } while (again == 'y' || again == 'Y');
+    writeToFile(notFound, notFoundCounter);
     return 0;
 }
 
@@ -124,7 +137,20 @@ int binarySearch(const string array[], int numElems, string value)
 // information in each array contained at the subscript.     *
 //************************************************************
 
-void displayProd(const double gpa[], int index)
+void displayProd(const double gpa[], int index, const string names[])
 {
+    cout << "Name: " << names[index] << "\n";
     cout << "GPA: " << gpa[index] << endl;
+}
+
+void writeToFile(const string names[], int count) {
+    ofstream outputFile ("missingNames.text");
+    if (outputFile.is_open()) {
+        for (int j = 0; j < count; j++) {
+            outputFile << names[j] + "\n";
+        }
+        outputFile.close();
+    } else {
+        cout << "Unable to create file to write missing names";
+    }
 }
