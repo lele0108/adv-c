e41b7dbfe1520985fcf6978e56d2157baf46c153
Jimmy Liu <sicong.liu98@gmail.com>
2015-10-03 16:39:42 -0700

    comments

--- Project-2/main.cpp
+++ Project-2/main.cpp
@@ -141,3 +141,64 @@ void printThis(const int storage[][30], int num) {
         cout << endl;
     }
 }
+
+/**
+ MacBook-Pro-2:project-2 jimmyliu$ ./a.out
+     | BUR FAT LGB LAX MRY OAK SMF SAN SFO SJC SBO SMO
+ ---   --- --- --- --- --- --- --- --- --- --- --- ---
+ BUR |   0   0   0   0 122   0   0   0   0   0 316   0
+ FAT |   0   0   0   0   0   0   0   0 321 455   0   0
+ LGB |   0   0   0 125   0   0   0   0 267   0   0   0
+ LAX |   0   0 125   0   0  50   0   0 250   0 370   0
+ MRY | 122   0   0   0   0 259   0   0   0   0   0 119
+ OAK |   0   0   0  50 259   0   0 129   0   0   0   0
+ SMF |   0   0   0   0   0   0   0 111   0   0 125   0
+ SAN |   0   0   0   0   0 129 111   0   0   0   0   0
+ SFO |   0 321 267 250   0   0   0   0   0 190   0   0
+ SJC |   0 455   0   0   0   0   0   0 190   0   0   0
+ SBO | 316   0   0 370   0   0 125   0   0   0   0 145
+ SMO |   0   0   0   0 119   0   0   0   0   0 145   0
+ 
+ BUR (2): MRY, SBO
+ FAT (2): SFO, SJC
+ LGB (2): LAX, SFO
+ LAX (4): LGB, OAK, SFO, SBO
+ MRY (3): BUR, OAK, SMO
+ OAK (3): LAX, MRY, SAN
+ SMF (2): SAN, SBO
+ SAN (2): OAK, SMF
+ SFO (4): FAT, LGB, LAX, SJC
+ SJC (2): FAT, SFO
+ SBO (4): BUR, LAX, SMF, SMO
+ SMO (2): MRY, SBO
+ 
+ BUR - MRY
+ BUR - SBO
+ FAT - SFO
+ FAT - SJC
+ LGB - LAX
+ LGB - SFO
+ LAX - OAK
+ LAX - SFO
+ LAX - SBO
+ MRY - OAK
+ MRY - SMO
+ OAK - SAN
+ SMF - SAN
+ SMF - SBO
+ SFO - SJC
+ SBO - SMO
+ 
+ BUR -> MRY 122
+ FAT -> SFO 321
+ LGB -> LAX 125
+ LAX -> OAK  50
+ MRY -> SMO 119
+ OAK -> LAX  50
+ SMF -> SAN 111
+ SAN -> SMF 111
+ SFO -> SJC 190
+ SJC -> SFO 190
+ SBO -> SMF 125
+ SMO -> MRY 119
+ */
