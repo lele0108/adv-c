de15081dc6e53905151cb590ce688a109cd09bb0
Jimmy Liu <sicong.liu98@gmail.com>
2015-10-17 22:36:43 -0700

    working

--- Project-3/main.cpp
+++ Project-3/main.cpp
@@ -14,10 +14,18 @@ using namespace std;
 double** readThisFile(double**);
 void printStuff(double**);
 void insertionSort(double[], int);
+double** tableSort(double**);
+int computeSize(double**);
 
 int main() {
 	double **pointers;
 	pointers = readThisFile(pointers);
+  int count = 0;
+  while (pointers[count] != NULL) {
+    insertionSort(pointers[count], pointers[count][0]);
+    count++;
+  }
+  pointers = tableSort(pointers);
   printStuff(pointers);
 	return 0;
 }
@@ -37,9 +45,8 @@ double** readThisFile(double **pointers) {
     	stringstream stream(line);
       stream >> number;
       pointers[count] = new double[(int)number + 1];
-      pointers[count][0] = number + 1;
+      pointers[count][0] = number;
     	while (stream >> number) {
-        //cout << number << " " << endl;
         pointers[count][innercounter] = number;
         innercounter++;
       }
@@ -54,17 +61,22 @@ double** readThisFile(double **pointers) {
 }
 
 void printStuff(double **pointers) {
+  int size = 0;
+  size = computeSize(pointers);
+  cout << "The ragged array has " << size << " rows:" << endl;
   int count = 0;
   while (pointers[count] != NULL) {
     int temp = pointers[count][0];
-    for (int j = 0; j < temp; j++) {
-      cout << pointers[count][j] << " ";
+    for (int j = 0; j < temp + 1; j++) {
+      if (j == 0) {
+        cout << setw(2) << fixed << setprecision(0) << pointers[count][j] << " ";
+      } else {   
+        cout << setw(5) << fixed << setprecision(1) << pointers[count][j] << " ";
+      }
     }
     cout << endl;
     count++;
   }
-}
-
 }
 
 //**************************************************************
@@ -75,15 +87,38 @@ void printStuff(double **pointers) {
 
 void insertionSort(double array[], int size)
 {
-   for (int curr = 1; curr < size; curr++)
-   {
+   for (int curr = 2; curr < size + 1; curr++) {
       double hold = array[curr];                   // copy current element to hold
       int back = curr - 1;
-      while (back >= 0 && hold > array[back])   // search where to insert the current element
+      while (back >= 1 && hold > array[back])   // search where to insert the current element
       {
           array[back + 1] = array[back];        // shift to the right
           back--;
       }
       array[back + 1] = hold;                   // put hold back to the array
    }
+}
+
+double** tableSort(double **pointers) {
+  int curr = 1;
+  while(pointers[curr] != NULL) {
+      double *hold = pointers[curr];                   // copy current element to hold
+      int back = curr - 1;
+      while (back >= 0 && hold[0] > pointers[back][0])   // search where to insert the current element
+      {
+          pointers[back + 1] = pointers[back];        // shift to the right
+          back--;
+      }
+      pointers[back + 1] = hold;                   // put hold back to the array
+      curr++;
+  }
+
+  return pointers;
+}
+
+int computeSize(double **pointers) {
+  int count = 0;
+  while(pointers[count] != NULL) 
+    count++;
+  return count;
 }
